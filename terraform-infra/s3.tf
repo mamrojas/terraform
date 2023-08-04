e930b0cf6a58bc579fea221bcda79fbf2f30f037
Marco Rojas <mmrojas@outlook.com>
2023-08-04 13:04:46 +0200

    Update s3.tf
    Updating it

--- terraform-infra/s3.tf
+++ terraform-infra/s3.tf
@@ -5,21 +5,21 @@ resource "aws_s3_bucket" "project_s3" {
 }
 
 #attaching a bucket policy to give terraform permissions
-#resource "aws_s3_bucket_policy" "bucket_policy" {
-#  bucket = "mrtfbucket1"
-#  policy = file("policy.json")
-#}
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = "mrtfbucket1"
+  policy = file("policy.json")
+}
 
 #enabeling bucket versioning for recovery
-#resource "aws_s3_bucket_versioning" "project_versioning" {
-#  bucket = "mrtfbucket1"
-#  versioning_configuration {
-#    status = "Enabled"
-#  }
-#}
+resource "aws_s3_bucket_versioning" "project_versioning" {
+  bucket = "mrtfbucket1"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 #setting the acl of the bucket to private
-#resource "aws_s3_bucket_acl" "project_acl" {
-#  bucket = "mrtfbucket1"
-#  acl    = "private"
-#}
+resource "aws_s3_bucket_acl" "project_acl" {
+  bucket = "mrtfbucket1"
+  acl    = "private"
+}
